cb660ba29ab3d5ab5c616ff2a162b4f309fba012
deathart <deathart@hotmail.fr>
2017-11-17 21:26:19 +0100

    Add layout translation

---
+++ application/Language/fr/blog/layout.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'search' => 'Recherche',
+    'search_label' => 'Mots clef à rechercher',
+    'search_btn' => 'Rechercher',
+    'menu_home' => 'Accueil',
+    'menu_cat' => 'Catégories',
+    'menu_contact' => 'Contact',
+    'menu_about' => 'Informations',
+    'footer_copy' => 'Copyright © 2017 :name, All Rights Reserved. Page chargée en 0.7196 secondes. Version : <a href="https://github.com/deathart/BlogCI4" target="_blank">1.0.0</a> / CI : <a href="https://github.com/bcit-ci/CodeIgniter4" target="_blank">4.0-dev</a>',
+    'eupopup_title' => 'Notre site utilise des cookies.',
+    'eupopup_body' => 'Certains cookies sont nécessaires au fonctionnement du site, tandis que d\'autres nous aident à améliorer l\'expérience utilisateur. En utilisant le site, vous acceptez l\'utilisation des cookies.',
+    'eupopup_close' => 'Fermer',
+    'eupopup_more' => 'En savoir plus'
+];
