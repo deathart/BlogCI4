0a233a61cd3a151d817ef30bc1e5a5ff9be5a54c
deathart <deathart@hotmail.fr>
2017-11-19 18:32:09 +0100

    Add search translate

---
+++ application/Language/en/blog/search.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Search',
+    'form_label' => 'Keywords',
+    'form_btn' => 'Search',
+    'tabs_title_art' => 'Articles',
+    'tabs_title_com' => 'Comments',
+    'tabs_content_art' => 'No items matching your search were found',
+    'tabs_content_com' => 'No comments matching your search were found'
+];
---
+++ application/Language/fr/blog/search.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Recherche',
+    'form_label' => 'Mots clefs',
+    'form_btn' => 'Rechercher',
+    'tabs_title_art' => 'Articles',
+    'tabs_title_com' => 'Commentaires',
+    'tabs_content_art' => 'Aucun article correspondant à votre recherche à été trouvé',
+    'tabs_content_com' => 'Aucun commentaires correspondant à votre recherche à été trouvé'
+];
