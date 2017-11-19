a69fd40a5a2be1fb7209dac8079a726eca30fdac
deathart <deathart@hotmail.fr>
2017-11-19 18:40:19 +0100

    Add cookies translate

---
+++ application/Language/en/blog/cookies.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'title' => 'Cookie Policy for',
+    'content' => '<p>What are the cookies?</p>
+        <p>As is common practice with almost all professional websites, this site uses cookies, which are tiny files downloaded to your computer, to improve your experience. This page describes the information we collect, how we use it and why we sometimes need to store cookies. We will also share how you can prevent these cookies from being stored, but this may degrade or "break" some elements of the site\'s functionality.</p>
+        <p>For more general information on cookies, see Wikipedia\'s article on HTTP cookies...</p>
+        <p>How we use cookies</p>
+        <p>We use cookies for various reasons detailed below. Unfortunately, in most cases, there are no standard industry options to disable cookies without completely disabling the features and functionality they add to this site. It is recommended to leave on all cookies if you do not know whether you need them or not in case they are used to provide a service you use.</p>
+        <p>Disabling cookies</p>
+        <p>You can prevent cookies from being set by adjusting your browser settings (see your browser help for instructions on how to do this). Be aware that disabling cookies will affect the functionality of this site and many other sites you visit. Disabling cookies will generally disable certain features and functions of this site. Therefore, it is recommended that you do not disable cookies.</p>
+        <p>The cookies we set</p>
+        <p>&nbsp;</p>
+        <p>In order to provide you with an excellent experience on this site, we provide the functionality to set your preferences about how this site works when you use it. To remember your preferences, we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.</p>
+        <p>&nbsp;</p>
+        <p>Third-party cookies</p>
+        <p>In special cases, we also use cookies provided by approved third parties. The following section details the third party cookies you may encounter on this site.</p>
+        <p>This site uses Google Analytics which is one of the most popular and reliable analytical solutions on the Web to help us understand how you use the site and how to improve your experience. These cookies can track things such as how much time you spend on the site and the pages you visit so that we can continue to produce attractive content.</p>
+        <p>For more information about Google Analytics cookies, please visit the official Google Analytics page.</p>
+        <p>From time to time, we test new features and make subtle changes to the way the site is delivered. When we test new features again, these cookies can be used to ensure that you receive a consistent experience on the site while ensuring that we understand which optimizations our users appreciate most.</p>
+        <p>The Google AdSense service we use to serve ads uses a DoubleClick cookie to serve more relevant ads on the Web and limit the number of times an ad is displayed.</p>
+        <p>For more information about Google AdSense, see the official Google AdSense Privacy FAQ.</p>
+        <p>We use advertisements to offset the operating costs of this site and provide funds for further development. The behavioural advertising cookies used by this site are designed to ensure that we provide you with the most relevant advertising if possible by anonymously monitoring your interests and presenting similar items that may be of interest.</p>
+        <p>In some cases, we may provide you with custom content based on what you tell us about yourself directly or indirectly by linking a social media account. These types of cookies simply enable us to provide you with content that we believe may be of interest to you.</p>
+        <p>Many partners advertise on our behalf and affiliate tracking cookies simply allow us to see if our customers have come to the site through one of our partner sites so that we can credit them appropriately and, if necessary, allow our affiliate partners to provide a bonus that they could provide you with a purchase.</p>
+        <p>We also use social media buttons and/or plugins on this site that allow you to connect to your social network in various ways. In order for them to work, the following social media sites include: Facebook, Twitter, will set cookies via our site that can be used to improve your profile on their site or contribute to the data they hold for various purposes described in their respective privacy policies.</p>
+        <p>&nbsp;</p>
+        <p>More Information</p>
+        <p>I hope this clarified things for you and, as mentioned earlier, if you don\'t know whether or not you need it, it is generally safer to leave cookies enabled in case it interacts with any of the features you use on our site. However, if you are looking for more information, you can contact us through one of our preferred contact methods.</p>'
+];
---
+++ application/Language/fr/blog/cookies.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'title' => 'Politique sur les cookies pour',
+    'content' => '<p>Quels sont les cookies?</p>
+        <p>Comme c\'est pratique habituelle avec presque tous les sites Web professionnels, ce site utilise des cookies, qui sont de minuscules fichiers téléchargés sur votre ordinateur, afin d\'améliorer votre expérience. Cette page décrit les informations recueillies, la façon dont nous l\'utilisons et pourquoi nous devons parfois stocker ces cookies. Nous partagerons également la façon dont vous pouvez éviter que ces cookies ne soient stockés, mais cela risque de dégrader ou de «briser» certains éléments de la fonctionnalité des sites.</p>
+        <p>Pour plus d\'informations générales sur les cookies, consultez l\'article de Wikipedia sur les cookies HTTP ...</p>
+        <p>Comment utilisons des cookies</p>
+        <p>Nous utilisons des cookies pour diverses raisons détaillées ci-dessous. Malheureusement, dans la plupart des cas, il n\'existe aucune option standard de l\'industrie pour désactiver les cookies sans désactiver complètement les fonctionnalités et les fonctionnalités qu\'ils ajoutent à ce site. Il est recommandé de laisser sur tous les cookies si vous ne savez pas si vous en avez besoin ou non au cas où ils sont utilisés pour fournir un service que vous utilisez.</p>
+        <p>Désactivation des cookies</p>
+        <p>Vous pouvez empêcher le réglage des cookies en ajustant les paramètres de votre navigateur (voir l\'aide de votre navigateur pour savoir comment le faire). Soyez conscient que la désactivation des cookies affectera la fonctionnalité de ce site et de nombreux autres sites que vous visitez. La désactivation des cookies entraînera généralement la désactivation de certaines fonctionnalités et fonctionnalités de ce site. Par conséquent, il est recommandé de ne pas désactiver les cookies.</p>
+        <p>Les cookies que nous fixons</p>
+        <p>&nbsp;</p>
+        <p>Afin de vous fournir une excellente expérience sur ce site, nous fournissons la fonctionnalité pour définir vos préférences quant à la façon dont ce site fonctionne lorsque vous l\'utilisez. Pour se souvenir de vos préférences, nous devons définir les cookies afin que ces informations puissent être appelées chaque fois que vous interagissez avec une page est affectée par vos préférences.</p>
+        <p>&nbsp;</p>
+        <p>Cookies tiers</p>
+        <p>Dans certains cas particuliers, nous utilisons également des cookies fournis par des tiers approuvés. La section suivante détaille les cookies de tiers que vous pourriez rencontrer sur ce site.</p>
+        <p>Ce site utilise Google Analytics qui est l\'une des solutions analytiques les plus répandues et les plus fiables sur le Web pour nous aider à comprendre comment vous utilisez le site et les moyens d\'améliorer votre expérience. Ces cookies peuvent suivre des choses telles que combien de temps vous dépensez sur le site et les pages que vous visitez afin que nous puissions continuer à produire du contenu attrayant.</p>
+        <p>Pour plus d\'informations sur les cookies Google Analytics, consultez la page officielle Google Analytics.</p>
+        <p>De temps en temps, nous testons de nouvelles fonctionnalités et apportons des modifications subtiles à la manière dont le site est livré. Lorsque nous testons encore de nouvelles fonctionnalités, ces cookies peuvent être utilisés pour vous assurer que vous recevez une expérience constante sur le site tout en veillant à ce que nous comprenions quelles optimisations nos utilisateurs apprécient le plus.</p>
+        <p>Le service Google AdSense que nous utilisons pour diffuser de la publicité utilise un cookie DoubleClick pour diffuser des annonces plus pertinentes sur le Web et limiter le nombre de fois qu\'une annonce est affichée.</p>
+        <p>Pour plus d\'informations sur Google AdSense, consultez la FAQ de confidentialité Google AdSense officielle.</p>
+        <p>Nous utilisons des publicités pour compenser les coûts de fonctionnement de ce site et fournir des fonds pour un développement ultérieur. Les cookies de publicité comportementale utilisés par ce site sont conçus pour vous assurer que nous vous fournissons les publicités les plus pertinentes si possible en surveillant anonymement vos intérêts et en présentant des éléments similaires qui pourraient être intéressants.</p>
+        <p>Dans certains cas, nous pouvons vous fournir un contenu personnalisé en fonction de ce que vous nous dites sur vous directement ou indirectement en liant un compte de médias sociaux. Ces types de cookies nous permettent simplement de vous fournir un contenu qui, selon nous, pourrait vous intéresser.</p>
+        <p>Plusieurs partenaires annoncent en notre nom et les cookies de suivi des affiliés nous permettent simplement de voir si nos clients sont venus sur le site par l\'intermédiaire de l\'un de nos sites partenaires afin que nous puissions les créditer de manière appropriée et, le cas échéant, permettre à nos partenaires affiliés de fournir un bonus qu\'ils pourraient Vous procurer un achat.</p>
+        <p>Nous utilisons également des boutons de médias sociaux et / ou des plugins sur ce site qui vous permettent de vous connecter à votre réseau social de diverses façons. Pour que ceux-ci fonctionnent, les sites de médias sociaux suivants incluent: Facebook, Twitter, établira des cookies via notre site qui peut être utilisé pour améliorer votre profil sur leur site ou contribuer aux données qu\'ils détiennent à diverses fins décrites dans leurs politiques de confidentialité respectives.</p>
+        <p>&nbsp;</p>
+        <p>Plus d\'information</p>
+        <p>J\'espère que cela vous a clarifié les choses et, comme cela a été mentionné précédemment, si vous ne savez pas si vous avez besoin ou pas, il est généralement plus sûr de laisser les cookies activés au cas où il interagirait avec l\'une des fonctionnalités que vous utilisez sur notre site. Toutefois, si vous cherchez encore plus d\'informations, vous pouvez nous contacter à travers une de nos méthodes de contact préférées.</p>'
+];
