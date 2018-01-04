703fb1edea9c4964c2ac21ad090d098daf1dad39
deathart <deathart@hotmail.fr>
2018-01-04 16:12:28 +0100

    Typo in captcha

--- application/Libraries/Captcha.php
+++ application/Libraries/Captcha.php
@@ -19,7 +19,7 @@ class Captcha
     /**
      * @var array
      */
-    private $calcul = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    private $calcul = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     /**
      * Captcha constructor.
